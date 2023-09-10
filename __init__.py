f172d91b5586eff9bdd933e88f133c6da5acd51a
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2023-09-10 14:42:55 -0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from time import sleep
 import kthread
 
 
-def _start_as_tread(file, mode, action, stoptrigger):
+def _start_as_tread(file, mode, action, stoptrigger, encoding, errors):
     t = kthread.KThread(
         target=_read_file_async,
         kwargs={
@@ -14,6 +14,8 @@ def _start_as_tread(file, mode, action, stoptrigger):
             "mode": mode,
             "action": action,
             "stoptrigger": stoptrigger,
+            "encoding": encoding,
+            "errors": errors,
         },
     )
     t.daemon = True
@@ -31,6 +33,8 @@ def _read_file_async(
     mode="r",
     action=lambda line: sys.stderr.write((str(line) + "\n")),
     stoptrigger: list | tuple = (False,),
+    encoding: str = "utf-8",
+    errors: str = "backslashreplace",
 ):
     while not os.path.exists(file):
         sleep(0.005)
@@ -39,18 +43,26 @@ def _read_file_async(
         async def rline(_file):
             _file.seek(0, 2)
             while not stoptrigger[-1]:
-                line = _file.readline().strip("\n\r")
+                line = _file.readline()
                 if not line:
                     await asyncio.sleep(0.01)
                     continue
                 yield line
 
-        with open(file, mode) as f:
-            reader_generator = rline(f)
+        if "b" in mode:
+            with open(file, mode) as f:
+                reader_generator = rline(f)
 
-            while not stoptrigger[-1]:
-                line = await anext(reader_generator)
-                action(line)
+                while not stoptrigger[-1]:
+                    line = await anext(reader_generator)
+                    action(line)
+        else:
+            with open(file, mode, encoding=encoding, errors=errors) as f:
+                reader_generator = rline(f)
+
+                while not stoptrigger[-1]:
+                    line = await anext(reader_generator)
+                    action(line)
 
     try:
         return asyncio.run(start_reading())
@@ -64,21 +76,27 @@ def read_async(
     mode: str = "r",
     action=lambda line: sys.stderr.write((str(line) + "\n")),
     stoptrigger: list | tuple = (False,),
+    encoding: str = "utf-8",
+    errors: str = "backslashreplace",
 ):
     """
-    This function `read_async` reads a file asynchronously during its execution while somewhere some other
-    process writes to the same file.
-    Parameters:
+    Read a file asynchronously and process its content.
+
+    Args:
         file (str): The path to the file to be read.
         asthread (bool, optional): If True, the function will start as a thread. Default is True.
         mode (str, optional): The mode in which the file is opened. Default is 'r' (read mode).
         action (function, optional): A function to be applied to each line of the file. Default is to write the line to stderr.
-        stoptrigger (list | tuple, optional): A list or tuple (better a list) that when its last element is True, the function will stop reading the file. Default is [False].
+        stoptrigger (list | tuple, optional): A list or tuple (preferably a list) that triggers the function to stop reading when its last element is True. Default is [False].
+        encoding (str, optional): The character encoding of the file. Default is 'utf-8'.
+        errors (str, optional): How to handle encoding errors. Default is 'backslashreplace'.
+
     Returns:
-        If asthread is True, it returns a KThread object. Otherwise, it returns the result of the `_read_file_async` function.
+        kthread.KThread or None: If as_thread is True, it returns a KThread object. Otherwise, it returns None.
+
     The advantage of this function is that it allows for real-time processing of the file while it is being written by another process.
-    This is particularly useful in scenarios where the file is continuously updated and the updates need to
-    be processed immediately, such as in log monitoring or real-time data analysis.
+    This is particularly useful in scenarios where the file is continuously updated, and the updates need to be processed immediately, such as in log monitoring or real-time data analysis.
+
     Example usage:
         from threading import Timer
         from time import time
@@ -86,7 +104,7 @@ def read_async(
         stoptrigger = [False,]
         t = read_async(
             file=newfile,
-            asthread=True,
+            as_thread=True,
             mode="r",
             action=lambda line: sys.stderr.write((str(line) + "\n")),
             stoptrigger=stoptrigger,
@@ -95,6 +113,13 @@ def read_async(
         os.system("ping 8.8.8.8 -n 10 > " + newfile + "")
     """
     if not asthread:
-        return _read_file_async(file, mode=mode, action=action)
+        return _read_file_async(
+            file,
+            mode=mode,
+            action=action,
+            stoptrigger=stoptrigger,
+            encoding=encoding,
+            errors=errors,
+        )
     else:
-        return _start_as_tread(file, mode, action, stoptrigger)
+        return _start_as_tread(file, mode, action, stoptrigger, encoding, errors)
